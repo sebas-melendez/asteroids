396fedd7fba32babe983b08c8e36a770949dd256
sebas-melendez <sebastianmelendeztab@hotmail.com>
2025-08-11 19:38:34 -0600

    Added pygame, opens empty black screen

---
+++ main.py
@@ -0,0 +1,20 @@
+
+import pygame
+from constants import *
+BLACK = (0,0,0)
+def main():
+    pygame.init
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT ))
+    while True:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
+        screen.fill(BLACK)
+        pygame.display.flip()
+
+
+
+if __name__ == "__main__":
+    main()
