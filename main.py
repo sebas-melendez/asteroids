1908933056876087f03aa1f93ff949995ab4e6d3
sebas-melendez <sebastianmelendeztab@hotmail.com>
2025-08-12 18:38:55 -0600

    Ahora esta madre gira

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 
 import pygame
 from constants import *
+from player import *
 BLACK = (0,0,0)
 def main():
     pygame.init
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT ))
+    clock = pygame.time.Clock()
+    my_player = Player(SCREEN_WIDTH / 2,SCREEN_HEIGHT / 2)
     while True:
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+
+        delta_time = clock.tick(60) / 1000   
         screen.fill(BLACK)
+        my_player.update(delta_time)
+        my_player.draw(screen)
         pygame.display.flip()
+        
 
 
 
